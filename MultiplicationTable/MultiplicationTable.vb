9d164ce79eb37a494e35b2f1e29892615a03b7fe
AlexWheelock <framedabyss8@gmail.com>
2024-01-29 08:55:08 -0700

    init

--- MultiplicationTable/MultiplicationTable.vb
+++ MultiplicationTable/MultiplicationTable.vb
@@ -1,6 +1,15 @@
-﻿Module MultiplicationTable
+﻿'Alex Wheelock
+'RCET0265
+'Multiplication Table
+'https://github.com/AlexWheelock/MultiplicationTable.git
+
+Module MultiplicationTable
 
     Sub Main()
+        Dim userInput As String
+        Dim usernumber As Integer
+
+        For row
 
     End Sub
 
