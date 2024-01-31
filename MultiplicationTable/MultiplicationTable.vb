00c9fd6d325ca19bbe5d9471c918fc300983bca4
AlexWheelock <130195109+AlexWheelock@users.noreply.github.com>
2024-01-31 14:50:06 -0700

    Working mostly

--- MultiplicationTable/MultiplicationTable.vb
+++ MultiplicationTable/MultiplicationTable.vb
@@ -6,41 +6,57 @@
 Module MultiplicationTable
 
     Sub Main()
+        Dim numbers(20, 20) As Integer
+        Dim temp As String
         Dim userInput As String
-        Dim usernumber As Integer
-        Dim boolean1 As Boolean = False
-
-        Dim numbers(9, 9) As Integer
-        Dim value As String
-
-        Console.WriteLine("What would you like the dimensions of the table to be?")
+        Dim userNumber As Integer
+        Dim validInput As Boolean
 
 
+        Console.WriteLine("Please enter the max value of the the table that you would like it to go to." & vbNewLine _
+                          & "Press " & Chr(34) & "Q" & Chr(34) & " at any time to quit.")
         Do
-            userInput = Console.ReadLine()
-            Try
-                usernumber = CInt(userInput)
-                boolean1 = True
-                Console.WriteLine($"Enjoy your {usernumber}x{usernumber} Multiplication Table!")
-            Catch ex As Exception
-                Console.WriteLine("Please enter an integer.")
-            End Try
-        Loop Until boolean1 = True
+            validInput = False
 
-        For row = 0 To usernumber
-            For column = 0 To usernumber
-                numbers(row, column) = row * column
-            Next
-        Next
+            'takes userInput, and tries to convert it to an integer
+            'Loops until there is a valid input containing an integer
+            Do
+                userInput = Console.ReadLine()
+                Try
+                    userNumber = CInt(userInput)
+                    validInput = True
+                    Console.WriteLine($"Enjoy your {userNumber} x {userNumber} Multiplication Table:")
+                Catch ex As Exception
+                    If userInput = "q" Then
+                        Console.Write("Have a nice day!")
+                        Console.Read()
+                        Exit Sub
+                    ElseIf userInput = "Q" Then
+                        Console.Write("Have a nice day!")
+                        Console.Read()
+                        Exit Sub
+                    Else Console.WriteLine("You entered " & Chr(34) & $"{userInput}" & Chr(34) & ", please enter an integer.")
+                    End If
+                End Try
+            Loop Until validInput
 
-        For row = 0 To usernumber
-            For column = 0 To 9
-                value = CStr(numbers(row, column)) & " |"
-                Console.Write(value.PadLeft(6))
+
+            For row = 1 To userNumber
+                For column = 1 To userNumber
+                    numbers(row, column) = row * column
+                Next
             Next
+
+            For row = 1 To userNumber
+                For column = 1 To userNumber
+                    temp = CStr(numbers(row, column))
+                    Console.Write(temp.PadLeft(6))
+                Next
+            Next
+
             Console.WriteLine()
-        Next
-        Console.Read()
+            Console.WriteLine("Please enter another max value to create another table, or press " & Chr(34) & "Q" & Chr(34) & " to quit.")
+        Loop
     End Sub
 
 End Module
